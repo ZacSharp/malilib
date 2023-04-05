9b3be6bbdc27508119e293e2a3d13bfdc90788d5
ZacSharp <68165024+ZacSharp@users.noreply.github.com>
2023-04-06 01:28:14 +0200

    Fix world render event

--- src/main/java/fi/dy/masa/malilib/ForgeRenderEventHandler.java
+++ src/main/java/fi/dy/masa/malilib/ForgeRenderEventHandler.java
@@ -22,17 +22,4 @@ class ForgeRenderEventHandler
     {
         ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderTooltipLast(event.getMatrixStack(), event.getStack(), event.getX(), event.getY());
     }
-
-    @SubscribeEvent
-    public void onRenderWorldLast(RenderWorldLastEvent event)
-    {
-        net.minecraft.client.Camera cam = net.minecraft.client.Minecraft.getInstance().gameRenderer.getMainCamera();
-        float yaw = cam.getYRot();
-        float pitch = cam.getXRot();
-        event.getMatrixStack().pushPose();
-//        event.getMatrixStack().glRotatef(pitch, 1, 0, 0);
-//        event.getMatrixStack().glRotatef(yaw+180, 0, 1, 0);
-        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderWorldLast(event.getMatrixStack(), event.getProjectionMatrix(), net.minecraft.client.Minecraft.getInstance());
-        event.getMatrixStack().popPose();
-    }
 }
